a254406b15ecacab068088d1d50e1a69db0d690b
Odair <odaircori@gmail.com>
2026-01-15 21:08:00 -0300

    Adicionando componente logo no Menu

---
+++ src/components/Logo/index.tsx
@@ -0,0 +1,7 @@
+import logo from '@/assets/logo.png'
+
+export function Logo(){
+    return (
+        <img src={logo} alt="" />
+    )
+}
--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,36 +1,41 @@
+import { Logo } from '../Logo';
 import styles from './styles.module.css';
 
 export function Menu() {
   return (
-    <nav className={styles.menu}>
-      <a className={styles.brand} href="#inicio">
-        MythBeer
-      </a>
-      <ul className={styles.menuList}>
-        <li>
-          <a href="#panteao" className={styles.menuLink}>
-            Panteão
-          </a>
-        </li>
-        <li>
-          <a href="#degustacao" className={styles.menuLink}>
-            Degustação
-          </a>
-        </li>
-        <li>
-          <a href="#colecao" className={styles.menuLink}>
-            Coleção
-          </a>
-        </li>
-        <li>
-          <a href="#contato" className={styles.menuLink}>
-            Contato
-          </a>
-        </li>
-      </ul>
-      <a className={styles.cta} href="#degustacao">
-        Agendar
-      </a>
-    </nav>
+    <>
+    <Logo/>
+      <nav className={styles.menu}>
+      
+{/*         <a className={styles.brand} href="#inicio">
+          MythBeer
+        </a> */}
+        <ul className={styles.menuList}>
+          <li>
+            <a href="#panteao" className={styles.menuLink}>
+              Panteão
+            </a>
+          </li>
+          <li>
+            <a href="#degustacao" className={styles.menuLink}>
+              Degustação
+            </a>
+          </li>
+          <li>
+            <a href="#colecao" className={styles.menuLink}>
+              Coleção
+            </a>
+          </li>
+          <li>
+            <a href="#contato" className={styles.menuLink}>
+              Contato
+            </a>
+          </li>
+        </ul>
+        <a className={styles.cta} href="#degustacao">
+          Agendar
+        </a>
+      </nav>
+    </>
   );
 }
